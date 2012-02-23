9c5ab0a46b527e61c20a88f97d6252e3d7e8b576
Radu Potop <radu@radu.(none)>
2012-02-23 12:59:41 +0200

    fixed index overflow in get_rand_str that caused XSS error

--- phpminiadmin.php
+++ phpminiadmin.php
@@ -1017,7 +1017,7 @@ function qstr($s){
 function get_rand_str($len){
  $result='';
  $chars=array("A","B","C","D","E","F","a","b","c","d","e","f",0,1,2,3,4,5,6,7,8,9);
- for($i=0;$i<$len;$i++) $result.=$chars[rand(0,count($chars))-1];
+ for($i=0;$i<$len;$i++) $result.=$chars[rand(0,count($chars)-1)];
  return $result;
 }
 
