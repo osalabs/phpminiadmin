b90f972d8a0fb4ffb9e1b1160ccf99d1855082d3
Gustavo Novaro <gnovaro@users.noreply.github.com>
2018-08-23 14:54:13 -0300

    Update phpminiadmin.php

--- phpminiadmin.php
+++ phpminiadmin.php
@@ -4,6 +4,7 @@
  (c) 2004-2018 Oleg Savchuk <osalabs@gmail.com> http://osalabs.com
  Light standalone PHP script for quick and easy access MySQL databases.
  http://phpminiadmin.sourceforge.net
+ 
  Dual licensed: GPL v2 and MIT, see texts at http://opensource.org/licenses/
 */
 $ACCESS_PWD=''; #!!!IMPORTANT!!! this is script access password, SET IT if you want to protect you DB from public access
